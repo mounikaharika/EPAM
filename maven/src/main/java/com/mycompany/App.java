79c2ceec6964e5b1db0f70faa83c48ba1a5efcdf
ADMIN <ADMIN@ADMIN-PC>
2019-03-19 19:08:48 +0530

    first commit

---
+++ maven/src/main/java/com/mycompany/App.java
@@ -0,0 +1,16 @@
+package com.mycompany;
+
+/**
+ * 
+ * @author Mounika Reddy
+ *
+ * Hello World!
+ *
+ */
+public class App {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World! ");
+	}
+}
---
+++ maven/src/test/java/com/mycompany/AppTest.java
@@ -0,0 +1,23 @@
+package com.mycompany;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * test for simple app
+ * @author Mounika Reddy
+ *
+ */
+public class AppTest {
+
+	/**
+	 * Rigorous Test :-)
+	 */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+     }
+
+}
